1e59e7026c8c47f86c0010f58dda2199eb58be8b
Arūnas Žilinskas <arunas.zilinskas@gmail.com>
2024-03-14 23:28:54 +0200

    fix4

--- mysite/baldai/models.py
+++ mysite/baldai/models.py
@@ -238,7 +238,7 @@ class Order(models.Model):
             try:
                 if platform.system() == 'Linux':
                     print("Running the script gpio_17pin.py on Linux", file=sys.stderr)
-                    subprocess.run(['sudo', 'python3', '/var/www/mysite/baldai/gpio_17pin.py'])
+                    subprocess.run(['sudo', '-u', 'www-data', 'python3', '/var/www/mysite/baldai/gpio_17pin.py'])
                 else:
                     print("Unknown operating system")
             except subprocess.CalledProcessError as e:
